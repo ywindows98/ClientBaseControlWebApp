a81d1c2410be216795e9eec7b9b29218064b6f9c
ywindows98 <124464870+ywindows98@users.noreply.github.com>
2024-09-09 16:27:30 +0200

    Initial models migration

---
+++ ClientBaseControlWebApp/Data/Migrations/20240909142318_AddInitialModel.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClientBaseControlWebApp.Data.Migrations
+{
+    public partial class AddInitialModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Appearances",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SkinType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EyeColor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HairColor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Capillaries = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SkinRedness = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Tan = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MembraneColor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NeedleType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Appearances", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Surname = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    InitialComment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    NumberOfProcedures = table.Column<int>(type: "int", nullable: false),
+                    AllergiesComment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    MainComment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    IndicationColor = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    AppearanceId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Clients_Appearances_AppearanceId",
+                        column: x => x.AppearanceId,
+                        principalTable: "Appearances",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ProcedureRecords",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ProcedureType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Comment = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ClientId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ProcedureRecords", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ProcedureRecords_Clients_ClientId",
+                        column: x => x.ClientId,
+                        principalTable: "Clients",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Clients_AppearanceId",
+                table: "Clients",
+                column: "AppearanceId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ProcedureRecords_ClientId",
+                table: "ProcedureRecords",
+                column: "ClientId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ProcedureRecords");
+
+            migrationBuilder.DropTable(
+                name: "Clients");
+
+            migrationBuilder.DropTable(
+                name: "Appearances");
+        }
+    }
+}
