b5e8ea5ffb856bd63b54e1572ca514e07b30941a
ywindows98 <124464870+ywindows98@users.noreply.github.com>
2024-09-09 16:34:52 +0200

    Initialize view and controller for clients

---
+++ ClientBaseControlWebApp/Controllers/ClientsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ClientBaseControlWebApp.Controllers
+{
+    public class ClientsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
